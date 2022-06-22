75cb2bf85c605ef73989876fc0b9851ce7374fa9
DEBORAH SORIANO <80492355+dsoriano89@users.noreply.github.com>
2022-06-22 18:01:44 -0300

    carousel-2
    correction

--- src/js/js.js
+++ src/js/js.js
@@ -11,18 +11,16 @@ const App = {
     // this.controllers.openModal(el)
     // })
 
-    // this.elements.root.appendChild(el)
-    // this.elements.root.appendChild(btn)
+    //this.elements.root.appendChild(el);
+    //this.elements.root.appendChild(btn);
 
     const imgs = [
       'https://www.selecoes.com.br/wp-content/uploads/2022/05/mini-croissant-id921299628.jpg',
       'https://media-cdn.tripadvisor.com/media/photo-s/10/02/a6/b5/caption.jpg',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTPPkY00Ar7omcpDVUpnZYGPcHi34Nbf3ZTQ&usqp=CAU',
-    ];
-
+    ]
     const el = this.controllers.createCard('Bread', '1,99', 'Honey', imgs);
-    this.elements.root.appendChild(el);
-
+    this.elements.root.appendChild(el); 
     console.log('End');
   },
 };
